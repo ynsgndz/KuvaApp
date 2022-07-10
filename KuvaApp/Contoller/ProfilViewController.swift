eee822d634af7d29e8de4195d177faca31d67e61
yunus gündüz <yns3gndz@hotmail.com.tr>
2022-07-11 00:28:17 +0300

    Design files uploaded

---
+++ KuvaApp/Contoller/ProfilViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ProfilViewController.swift
+//  KuvaApp
+//
+//  Created by Yunus Gündüz on 10.07.2022.
+//
+
+import UIKit
+
+class ProfilViewController: UIViewController {
+
+    @IBOutlet weak var labelProfilDetayli: UILabel!
+    @IBOutlet weak var labelProfilDetayli2: UILabel!
+    @IBOutlet weak var labelProfilDetayli3: UILabel!
+    @IBOutlet weak var labelProfilDetayli4: UILabel!
+    @IBOutlet weak var labelProfilDetayli5: UILabel!
+    @IBOutlet weak var labelProfilDetayli6: UILabel!
+    @IBOutlet weak var labelProfilDetayli7: UILabel!
+    @IBOutlet weak var labelProfilDetayli8: UILabel!
+    @IBOutlet weak var labelProfilDetayli9: UILabel!
+    @IBOutlet weak var labelProfilDetayli10: UILabel!
+    @IBOutlet weak var labelProfilDetayli11: UILabel!
+    @IBOutlet weak var labelProfilDetayli12: UILabel!
+    @IBOutlet weak var labelProfilDetayli13: UILabel!
+    @IBOutlet weak var labelProfilDetayli14: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        labelProfilDetayli.text="Irk : Savaşçı"
+        labelProfilDetayli2.text="ID: 0001"
+        labelProfilDetayli3.text="Adi: Ynsgndz"
+        labelProfilDetayli4.text="Kademe: 1"
+        labelProfilDetayli5.text="Saldiri Gücü: 11"
+        labelProfilDetayli6.text="Güç: 5"
+        labelProfilDetayli7.text="Savunma: 5"
+        labelProfilDetayli8.text="Tecrube: 5"
+        labelProfilDetayli9.text="Yaşam Gücü: 100"
+        labelProfilDetayli10.text="Altın: 100"
+        labelProfilDetayli11.text="Toplam Savaş: 4"
+        labelProfilDetayli12.text="Galibiyet: 3"
+        labelProfilDetayli13.text="Yenilgi: 1"
+        labelProfilDetayli14.text="Beraberlik: 0"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
