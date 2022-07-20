6c8b68ef5ae5a7387ccb76625ec715ab11ff696b
yunus gündüz <yns3gndz@hotmail.com.tr>
2022-07-20 16:18:08 +0300

    full veri çekme for

--- KuvaApp/Main/ViewController.swift
+++ KuvaApp/Main/ViewController.swift
@@ -22,9 +22,50 @@ class ViewController: UIViewController {
         if  username == "ynsgndz"{
             performSegue(withIdentifier: "loginToMain", sender: nil)
         }
-        //
-        bilgiCekme()
+        tryJsonCekme()
+      //  bilgiCekme()
     }
+    func tryJsonCekme(){
+        print("Try")
+     
+        let url = URL(string: "http://yunusgunduz.site/public/api/")
+        let session  = URLSession.shared
+        let task = session.dataTask(with: url!) { data, response, error in
+            if error != nil {
+                print(error?.localizedDescription as Any)
+            }else{
+                if data != nil {
+                    do {
+                        let jsonResponse = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers)
+                        as? [Dictionary<String, Any>]
+                        DispatchQueue.main.async {
+                           
+                           // print(jsonResponse!)
+                            
+                            for i in jsonResponse!{
+                                
+                                if  i["username"]! as! String == "ynsgndz"{
+                                    print(i["username"]!)
+                                   
+                                }
+                            }
+                            
+                           
+                            
+                         
+                           
+                          
+                                
+                        }
+                    }catch{
+                        print(error)
+                    }
+                }
+            }
+            
+        }.resume()
+    }
+    
     
     func bilgiCekme(){
         let username = usernameTextField.text!
